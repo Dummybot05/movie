119c05a3156045dbd8a45b789fc6ec9f9774195e
Steel Troops <117297525+Dummybot05@users.noreply.github.com>
2024-08-02 11:13:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,30 +10,7 @@ app.use(express.static('public'));
 
 var links = data.links;
 var actual = data.actual;
-/*
-var links = [
-    'Godzilla_x_Kong:_The_New_Empire',
-    'Kushi_(2023_film)',
-    'Leo_(2023_Indian_film)'
-];
-var actual = {
-    'Godzilla_x_Kong:_The_New_Empire' : [
-      'https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720&autoplay=true&mute=false&uk=4398578630871&fid=660742023789616&slid=',
-      'https://www.youtube.com/watch?v=qqrpMRDuPfc',
-      'https://1024terabox.com/s/155qDWD_Me3OfL_XAElCKog'
-    ],
-    'Kushi_(2023_film)': [
-      'https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080&autoplay=true&mute=false&uk=4398578630871&fid=862428064770165&slid=',
-      'https://www.youtube.com/watch?v=AsvGScyj4gw',
-      'https://1024terabox.com/s/1ryHvko7Qmu5kNHAu2JqnCA'
-    ],
-    'Leo_(2023_Indian_film)': [
-      'https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080&autoplay=true&mute=false&uk=4398578630871&fid=328192136908308&slid=',
-      'https://www.youtube.com/watch?v=ozRCVFgsrbY',
-      'https://1024terabox.com/s/1nFQnHjz_erzn7lsPGPKTmg'
-    ]
-};
-*/
+
 app.get('/', async (req, res) => {
   const data = [];
   const fetchDataPromises = links.map(async (itms) => {
@@ -63,7 +40,6 @@ app.get('/:index', async (req, res) => {
     const summary = await page.summary();
     const infobox = await page.infobox();
     let ddd = req.params.index;
-    console.log(actual[ddd])
     res.render('show.ejs', {
       actual: actual[ddd],
       path: req.params.index,
