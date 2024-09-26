053b2cd9a2df854e0fbd804abefee1c21db90bf8
dummybot0505 <dummybot0505@gmail.com>
2024-09-26 08:19:33 +0000

    modified:   data.js 	modified:   index.js 	modified:   views/home.ejs

--- data.js
+++ data.js
@@ -7,22 +7,22 @@ var links = [
 
 var actual = {
     'Animal_(2023_Indian_film)': [
-      'https://1024terabox.com/sharing/embed?surl=VMtDimKfOFm50wR50-vBag&resolution=1080&autoplay=true&mute=false&uk=4398578630871&fid=879038689017190&slid=',
-      'https://youtu.be/YD7b4gU0HWQ',
+      'https://1024terabox.com/sharing/embed?surl=VMtDimKfOFm50wR50-vBag&resolution=1080',
+      'https://www.youtube.com/watch?v=YD7b4gU0HWQ',
       'https://1024terabox.com/s/1vMndjMHdW8YgYLwJxh9eIA'
     ],
     'Godzilla_x_Kong:_The_New_Empire' : [
-      'https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720&autoplay=true&mute=false&uk=4398578630871&fid=660742023789616&slid=',
+      'https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720',
       'https://www.youtube.com/watch?v=qqrpMRDuPfc',
       'https://1024terabox.com/s/155qDWD_Me3OfL_XAElCKog'
     ],
     'Kushi_(2023_film)': [
-      'https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080&autoplay=true&mute=false&uk=4398578630871&fid=862428064770165&slid=',
+      'https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080',
       'https://www.youtube.com/watch?v=AsvGScyj4gw',
       'https://1024terabox.com/s/1ryHvko7Qmu5kNHAu2JqnCA'
     ],
     'Leo_(2023_Indian_film)': [
-      'https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080&autoplay=true&mute=false&uk=4398578630871&fid=328192136908308&slid=',
+      'https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080',
       'https://www.youtube.com/watch?v=ozRCVFgsrbY',
       'https://1024terabox.com/s/1nFQnHjz_erzn7lsPGPKTmg'
     ]
