8250388a79088e807275d417b6a691c4d47e2439
dummybot0505 <dummybot0505@gmail.com>
2024-09-26 14:59:41 +0000

    Total 5 Movies

--- data.js
+++ data.js
@@ -4,7 +4,8 @@ const dataArray = [
     "links": [
       "https://www.youtube.com/watch?v=YD7b4gU0HWQ",
       "https://1024terabox.com/s/1vMndjMHdW8YgYLwJxh9eIA",
-      "https://1024terabox.com/sharing/embed?surl=VMtDimKfOFm50wR50-vBag&resolution=1080"
+      "https://1024terabox.com/sharing/embed?surl=VMtDimKfOFm50wR50-vBag&resolution=1080",
+      "Telugu"
     ]
   },
   {
@@ -12,7 +13,8 @@ const dataArray = [
     "links": [
       "https://youtube.com/watch?v=Uh6sp7CHsx8",
       "https://1024terabox.com/s/1wPIZYy6JAusYDHtCtDzxUw",
-      "https://1024terabox.com/sharing/embed?surl=Ay7kKzy5M1CUJa5prEP_DQ&resolution=360"
+      "https://1024terabox.com/sharing/embed?surl=Ay7kKzy5M1CUJa5prEP_DQ&resolution=1080",
+      "Telugu"
     ]
   },
   {
@@ -20,7 +22,8 @@ const dataArray = [
     "links": [
       "https://www.youtube.com/watch?v=qqrpMRDuPfc",
       "https://1024terabox.com/s/155qDWD_Me3OfL_XAElCKog",
-      "https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720"
+      "https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720",
+      "Telugu"
     ]
   },
   {
@@ -28,7 +31,8 @@ const dataArray = [
     "links": [
       "https://www.youtube.com/watch?v=AsvGScyj4gw",
       "https://1024terabox.com/s/1ryHvko7Qmu5kNHAu2JqnCA",
-      "https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080"
+      "https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080",
+      "Telugu"
     ]
   },
   {
@@ -36,7 +40,8 @@ const dataArray = [
     "links": [
       "https://www.youtube.com/watch?v=ozRCVFgsrbY",
       "https://1024terabox.com/s/1nFQnHjz_erzn7lsPGPKTmg",
-      "https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080"
+      "https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080",
+      "Telugu"
     ]
   }
 ];
