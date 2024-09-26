36be5df2a267268019edded052804fea42182939
dummybot0505 <dummybot0505@gmail.com>
2024-09-26 12:02:23 +0000

    update

--- data.js
+++ data.js
@@ -1,32 +1,28 @@
-var links = [
+const data = [
+  [
     'Animal_(2023_Indian_film)',
+    'https://www.youtube.com/watch?v=YD7b4gU0HWQ',
+    'https://1024terabox.com/s/1vMndjMHdW8YgYLwJxh9eIA',
+    'https://1024terabox.com/sharing/embed?surl=VMtDimKfOFm50wR50-vBag&resolution=1080'
+  ],
+  [
     'Godzilla_x_Kong:_The_New_Empire',
+    'https://www.youtube.com/watch?v=qqrpMRDuPfc',
+    'https://1024terabox.com/s/155qDWD_Me3OfL_XAElCKog',
+    'https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720',
+  ],
+  [
     'Kushi_(2023_film)',
-    'Leo_(2023_Indian_film)'
+    'https://www.youtube.com/watch?v=AsvGScyj4gw',
+    'https://1024terabox.com/s/1ryHvko7Qmu5kNHAu2JqnCA',
+    'https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080'
+  ],
+  [
+    'Leo_(2023_Indian_film)',
+    'https://www.youtube.com/watch?v=ozRCVFgsrbY',
+    'https://1024terabox.com/s/1nFQnHjz_erzn7lsPGPKTmg',
+    'https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080'
+  ]
 ];
 
-var actual = {
-    'Animal_(2023_Indian_film)': [
-      'https://1024terabox.com/sharing/embed?surl=VMtDimKfOFm50wR50-vBag&resolution=1080',
-      'https://www.youtube.com/watch?v=YD7b4gU0HWQ',
-      'https://1024terabox.com/s/1vMndjMHdW8YgYLwJxh9eIA'
-    ],
-    'Godzilla_x_Kong:_The_New_Empire' : [
-      'https://1024terabox.com/sharing/embed?surl=_SIjnDU0Cvi9d5Kg_gpkZw&resolution=720',
-      'https://www.youtube.com/watch?v=qqrpMRDuPfc',
-      'https://1024terabox.com/s/155qDWD_Me3OfL_XAElCKog'
-    ],
-    'Kushi_(2023_film)': [
-      'https://1024terabox.com/sharing/embed?surl=7-X8PBOqWTrwj10-jgHnVw&resolution=1080',
-      'https://www.youtube.com/watch?v=AsvGScyj4gw',
-      'https://1024terabox.com/s/1ryHvko7Qmu5kNHAu2JqnCA'
-    ],
-    'Leo_(2023_Indian_film)': [
-      'https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080',
-      'https://www.youtube.com/watch?v=ozRCVFgsrbY',
-      'https://1024terabox.com/s/1nFQnHjz_erzn7lsPGPKTmg'
-    ]
-}
-
-exports.links = links;
-exports.actual = actual;
+exports.data = data;
--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-const express = require('express');
-const PORT = process.env.PORT || 3000;
-const app = express();
 const wiki = require('wikipedia');
+const express = require('express');
+const app = express();
 const data = require('./data.js');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var links = data.links;
-var actual = data.actual;
-
-app.get('/', async (req, res) => {
-  const data = [];
-  const fetchDataPromises = links.map(async (itms) => {
-    try {
-      const page = await wiki.page(itms);
-      const summary = await page.summary();
-      const infobox = await page.infobox();
-      return [itms, summary, infobox];
-    } catch (error) {
-      console.log(error);
-      return [itms, 'Error fetching data', 'Error fetching data'];
-    }
-  });
-
+const renderData = [];
+data.data.map(async (item) => {
   try {
-    const results = await Promise.all(fetchDataPromises);
-    res.render('index.ejs', { list: results });
+    const page = await wiki.page(item[0]);
+    const summary = await page.summary();
+    const infobox = await page.infobox();
+    renderData.push([item, summary, infobox]);
+    console.log(renderData[renderData.length - 1][0]["Leo_(2023_Indian_film)"]);
   } catch (error) {
-    console.log('Error processing data:', error);
-    res.status(500).send('Internal Server Error');
+    console.log(error);
   }
+})
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { data: renderData });
+  
 });
+
+renderData.map((item) => {
+  console.log(item);
+});
+
+async function fetch2(index) {
+  try {
+    const page = await wiki.page(index);
+    const summary = await page.summary();
+    const infobox = await page.infobox();
+    return [summary, infobox];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 app.get('/:index', async (req, res) => {
   try {
-    const page = await wiki.page(req.params.index);
-    const summary = await page.summary();
-    const infobox = await page.infobox();
-    let ddd = req.params.index;
-    res.render('show.ejs', {
-      actual: actual[ddd],
-      path: req.params.index,
-      summary: summary,
-      infobox: infobox
+    fetch2(req.params.index).then((data) => {
+      res.render('show.ejs', { summary: data[0], infobox: data[1], actual: renderData[req.params.index] });
     });
   } catch (error) {
-    console.error('Error fetching page data:', error);
-    res.status(500).send('Internal Server Error');
+    console.log(error);
   }
 });
+
+
 
 app.listen(PORT, () => { console.log("Started..."); });
