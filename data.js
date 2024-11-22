a6fecccd3f4aaf2ee0fc885c60663b90ffb5ff57
Dummybot05 <dummybot0505@gmail.com>
2024-11-22 19:20:57 +0530

    Your descriptive commit message

--- data.js
+++ data.js
@@ -52,9 +52,83 @@ const dataArray = [
       "https://1024terabox.com/sharing/embed?surl=73BBkSwE6-uLo6djsCLbAw&resolution=1080",
       "Telugu"
     ]
-  }
+  },
+  {
+    "title": "C.D_(Criminal_or_Devil)",
+    "links": [
+      "https://www.youtube.com/watch?v=3hT24Zzcu1U",
+      "https://1024terabox.com/s/10DmIjYMDyjcsz9Cm7scFFg",
+      "https://1024terabox.com/sharing/embed?surl=Q3dU16mrEIoxAIAyKgDsiA&resolution=480",
+      "Telugu"
+    ]
+  },
+  {
+   "title": "ARM_(film)",
+   "links": [
+     "https://www.youtube.com/watch?v=sudjm_tz5Fc",
+     "https://1024terabox.com/s/197MHt3JZI9ajpFh1W9Thcg",
+     "https://1024terabox.com/sharing/embed?surl=9uxhFftFCn8dhlT6yLpjfQ&resolution=480",
+     "Telugu"
+   ]
+  },
+  {
+   "title": "Maa_Nanna_Superhero",
+   "links": [
+     "https://www.youtube.com/watch?v=btV5vOD2LP8",
+     "https://1024terabox.com/s/1tPEy-mJLLiiiRnrqdm5nfw",
+     "https://1024terabox.com/sharing/embed?surl=LgNmZEBpWsClgyRxbzjg7g&resolution=480",
+     "Telugu"
+   ]
+  },
+  {
+   "title": "Vettaiyan",
+   "links": [
+     "https://www.youtube.com/watch?v=I2VQenNOWOU",
+     "https://1024terabox.com/s/1JhlEDRFWrfZlRFCaOeY2Dg",
+     "https://1024terabox.com/sharing/embed?surl=9yRIoSUc6lEPZFFY_SSu8g&resolution=480",
+     "Telugu"
+   ]
+  },
+  {
+   "title": "Hitler_(2024_film)",
+   "links": [
+     "https://www.youtube.com/watch?v=HrI4Eg0VCjY",
+     "https://1024terabox.com/s/1d2QHXVvSNVeaSYNYxBjR0w",
+     "https://1024terabox.com/sharing/embed?surl=77JQDwnhZoRzdPDGLfW9zw&resolution=480",
+     "Telugu"
+   ]
+  },
+  {
+   "title": "Cinderella_(2021_Indian_film)",
+   "links": [
+     "https://www.youtube.com/watch?v=XTGF9ApqSOg",
+     "https://1024terabox.com/s/1uHW9WYI6viuMYFM5mUqr3w",
+     "https://1024terabox.com/sharing/embed?surl=TVN3_MLeRAtw3CtXIKf1Cw&resolution=1080",
+     "Telugu"
+   ]
+  },
+  {
+   "title": "Meiyazhagan",
+   "links": [
+     "https://www.youtube.com/watch?v=KJtYvw5DakQ",
+     "https://1024terabox.com/s/1xlemsLU_kNZbYnpVZznCSQ",
+     "https://1024terabox.com/sharing/embed?surl=WbwifxnaUYL1U7rcTsjjsA&resolution=480",
+     "Telugu"
+   ]
+  },
+  {
+   "title": "Luck_(2022_film)",
+   "links": [
+     "https://www.youtube.com/watch?v=xSG5UX0EQVg",
+     "https://1024terabox.com/s/1EkUBT0aDPce0SJeuLM3nfw",
+     "https://1024terabox.com/sharing/embed?surl=Yl4wt30vwrbgApWY5v7-SQ&resolution=360",
+     "Hindi"
+   ]
+  },
 ];
 
 const dataMap = Object.fromEntries(dataArray.map(item => [item.title, item.links]));
 
 module.exports = { dataArray, dataMap }
+
+
